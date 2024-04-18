f66083683f6507221b7b608e3050c528be65e728
sepega1337 <166050421+sepega1337@users.noreply.github.com>
2024-04-18 20:33:00 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from pygame import *
+
 init()
 display.set_mode((700, 500))
 fps = time.Clock()
@@ -6,43 +7,76 @@ window = display.set_mode((700,500))
 window.fill((200,20,100))
 bg = transform.scale(image.load('5D__3254.jpg'),(700,500))
 
-sprite1 = transform.scale(image.load('безымянный.png'),(20,200))
-hitbox_sprit1 = sprite1.get_rect()
-hitbox_sprit1.x = 0
-hitbox_sprit1.x = 650
+class Rocket():
+    def __init__(self, img, x, y, width_rocket, hight_rocket, speed):
+        self.image = transform.scale(image.load(img), (width_rocket, hight_rocket))
+        self.image_hit = self.image.get_rect()
+        self.image_hit.x = x
+        self.image_hit.y = y
+        self.speed = speed
 
-sprite2 = transform.scale(image.load('безымянный.png'),(20,200))
-hitbox_sprit2 = sprite2.get_rect()
-hitbox_sprit2.x = 0
-hitbox_sprit2.x = 50
+    def show_s(self):
+        window.blit(self.image, (self.image_hit.x, self.image_hit.y))
+
+class Rocket_player2(Rocket):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] :
+            self.image_hit.y -= self.speed
+        if keys[K_DOWN] :
+            self.image_hit.y += self.speed
+
+class Rocket_player1(Rocket):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] :
+            self.image_hit.y -= self.speed
+        if keys[K_s] :
+            self.image_hit.y += self.speed
+
+        
+
+rocket1 = Rocket_player1('безымянный.png', 30, 200, 25, 250, 6)
+rocket2 = Rocket_player2('безымянный.png', 650, 200, 25, 250, 6)
+ball = Rocket('image-removebg-preview.png', 200, 200, 50, 50, 7)
+
+
+
+
+fps = time.Clock()
 
 run = True
-
+speed_x = 3
+speed_y = 3
 while run:
     for i in event.get():
         if i.type == QUIT:
             run = False
-        if i.type == KEYDOWN:
-            if i.key == K_w:
-                hitbox_sprit1.y -= 20
-            if i.key == K_s:
-                hitbox_sprit1.y += 20
+    
 
-#спрайт 2:
-            if i.key == K_DOWN:
-                hitbox_sprit2.y += 20
-            if i.key == K_UP:
-                hitbox_sprit2.y -= 20
+    if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+        speed_x *= -1
+        speed_y *= -1
 
-
-            
-
+    if ball.image_hit.y < 10:
+        speed_y *= -1
+    
+    if ball.image_hit.y > 470:
+        speed_y *= -1
 
 
 
 
     window.blit(bg,(0, 0))
-    window.blit(sprite1,hitbox_sprit1)
-    window.blit(sprite2,hitbox_sprit2)
+
+    ball.image_hit.x += speed_x
+    ball.image_hit.y += speed_y
+
+
+    rocket1.show_s( )
+    rocket1.update_r()
+    rocket2.show_s( )
+    rocket2.update_r()
+    ball.show_s()
     display.update()
     fps.tick(60)
